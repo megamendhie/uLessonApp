9d9f6b01fbeeaaa540e8bcbf04853b6439908736
megamendhie <imendhie@yahoo.com>
2021-09-16 12:47:21 +0100

    updated serialized names

--- app/src/main/java/com/test/ulesson/models/Lesson.kt
+++ app/src/main/java/com/test/ulesson/models/Lesson.kt
@@ -9,17 +9,18 @@ data class Lesson(val success: Boolean = false, val data: List<Data>)
 
 @Entity(tableName = "myLessons_table")
 data class Data(
-    @PrimaryKey
+        @PrimaryKey
     val id: String,
-    @SerializedName("image_url") val imageUrl: String,
-    @SerializedName("start_at") val startAt: String,
-    @SerializedName("expires_at") val expiresAt: String,
-    @Embedded
-    val tutot: Tutot,
-    @Embedded
+        @SerializedName("image_url") val imageUrl: String,
+        @SerializedName("start_at") val startAt: String,
+        @SerializedName("expires_at") val expiresAt: String,
+        @Embedded
+    val tutor: Tutor,
+        @Embedded
     val subject: Subject,
-    var status: String, val topic: String, val createdAt: String)
+        var status: String, val topic: String, val createdAt: String)
 
-data class Tutot(val firstname: String, val lastname: String)
+data class Tutor(@SerializedName("firstname") val firstName: String?,
+                 @SerializedName("lastname") val lastName: String?)
 
 data class Subject(val name: String)
