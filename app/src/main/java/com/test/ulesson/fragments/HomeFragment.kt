825fdbc7c783c5ccbc104a3600b0264ec28af9f4
megamendhie <imendhie@yahoo.com>
2021-09-17 06:19:01 +0100

    bind live data to views

--- app/src/main/java/com/test/ulesson/fragments/HomeFragment.kt
+++ app/src/main/java/com/test/ulesson/fragments/HomeFragment.kt
@@ -1,58 +1,47 @@
 package com.test.ulesson.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.test.ulesson.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.test.ulesson.adapters.LessonPagerAdapter
+import com.test.ulesson.adapters.LiveLessonsAdapter
+import com.test.ulesson.databinding.FragmentHomeBinding
+import com.test.ulesson.models.Data
+import com.test.ulesson.viewmodels.LessonsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: LessonsViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.lstLiveLessons.layoutManager = LinearLayoutManager(requireContext())
+
+
+        val action = HomeFragmentDirections.actionHomeFragmentToMyLessonsFragment()
+        binding.fabLesson.setOnClickListener {
+            findNavController().navigate(action)
+        }
+        viewModel.liveLessonsTop.observe(viewLifecycleOwner, { lessons ->  setLiveLessonHeader(lessons.data.take(3))})
+        viewModel.liveLessonsBottom.observe(viewLifecycleOwner, { lessons ->  setLiveLessonList(lessons.data)})
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                HomeFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    private fun setLiveLessonHeader(data: List<Data>){
+        binding.lessonsViewPager.adapter = LessonPagerAdapter(requireContext(), data)
+        binding.indicator.setupWithViewPager(binding.lessonsViewPager)
+    }
+
+    private fun setLiveLessonList(data: List<Data>){
+        binding.lstLiveLessons.adapter = LiveLessonsAdapter(data)
     }
 }
